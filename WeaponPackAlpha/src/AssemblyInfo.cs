814a06bb789da8b098081bdcbbce78c2d75b1afe
Filip Nowak <fipil@spincoders.com>
2024-01-14 12:23:43 +0100

    Upgraded to .NET7 and 1.19

--- WeaponPackAlpha/src/AssemblyInfo.cs
+++ WeaponPackAlpha/src/AssemblyInfo.cs
@@ -1,14 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CustomWeaponSpeed")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomWeaponSpeed")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ab6d85cc-c40f-4249-ba21-aa148a8c1c14")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
